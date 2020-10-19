33cd1c95128a804943f347620ce7e18c96272c06
Sin Wildy <sw.wildy@gmail.com>
2020-10-19 23:26:11 +0000

    Done.

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,8 +1,13 @@
 # don't forget to add: require 'pry'
 
+#require 'pry' 
+
 def snake_it_up(string)
   if string[0] == "s"
-    10 * "s" + string
+#    binding.pry
+#    10.times do "s" end + string 
+#    "ssssssssss" + string  
+     string.rjust(19, "s")
   else
     string
   end
