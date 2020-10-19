41a21cde8f06e8202f69e034851e33387a8fa63c
Sin Wildy <sw.wildy@gmail.com>
2020-10-19 23:23:57 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,5 +1,5 @@
 # don't forget to add: require 'pry'
-
+require 'pry'
 def generate_star_date
   (rand(100000) + 400000) / 10.0
 end
@@ -13,5 +13,6 @@ def engage
 #  date = generate_star_date
 #  puts state_log(date)
    star_date = generate_star_date
+   binding.pry
    puts state_log(star_dates)
 end
