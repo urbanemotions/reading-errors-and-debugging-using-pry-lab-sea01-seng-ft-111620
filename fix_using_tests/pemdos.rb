a80d2880b3cfa93eefc9384a9d2d3d157b2a030f
Sin Wildy <sw.wildy@gmail.com>
2020-10-19 23:05:51 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,15 +1,13 @@
 # don't forget to add: require 'pry'
 
-require 'pry' 
+#require 'pry' 
 
 def snake_it_up(string)
   if string[0] == "s"
 #    binding.pry
-#    10 * "s" + string
-#     10.times do "s" end + string 
-#    10.times do print "s" end + string
+#    10.times do "s" end + string 
 #    "ssssssssss" + string  
-    string.rjust(19, "s")
+     string.rjust(19, "s")
   else
     string
   end
